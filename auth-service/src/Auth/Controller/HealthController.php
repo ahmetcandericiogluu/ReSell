523ca11487de1ebf2d045a41292cb92571ce2296
ahmetcandericiogluu <4hcan2001@gmail.com>
2025-12-06 12:46:50 +0300

    feat: Add /health endpoint for auth-service Render health checks

---
+++ auth-service/src/Auth/Controller/HealthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Auth\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthController extends AbstractController
+{
+    #[Route('/health', name: 'health', methods: ['GET'])]
+    public function health(): JsonResponse
+    {
+        return $this->json([
+            'status' => 'healthy',
+            'service' => 'auth-service',
+            'timestamp' => time()
+        ]);
+    }
+}
+
