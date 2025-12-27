bc36c098527f0c89ee47996800b160b344796d17
ahmetcandericiogluu <4hcan2001@gmail.com>
2025-12-28 01:24:35 +0300

    .

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -12,5 +12,4 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
