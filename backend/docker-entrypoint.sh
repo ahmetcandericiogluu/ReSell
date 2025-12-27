324720fd46b465f6b1e1130c9568d88f0e193841
ahmetcandericiogluu <4hcan2001@gmail.com>
2025-12-28 00:18:30 +0300

    webprofilerbundle removed

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -21,9 +21,9 @@ sed -ri "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/g" /etc/apache2/sites-ava
 echo "ServerName localhost" > /etc/apache2/conf-available/servername.conf
 a2enconf servername >/dev/null 2>&1 || true
 
-# Clear and warmup cache for production
-rm -rf var/cache/prod/* 2>/dev/null || true
-php bin/console cache:clear --env=prod 2>&1 || true
+# Aggressively clear cache
+rm -rf var/cache/* var/log/* 2>/dev/null || true
+php bin/console cache:clear --env=prod --no-warmup 2>&1 || true
 php bin/console cache:warmup --env=prod 2>&1 || true
 
 # Set permissions
